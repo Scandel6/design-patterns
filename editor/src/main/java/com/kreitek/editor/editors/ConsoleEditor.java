f01c7a83e8f9e89c4b4552e2e601a2a101b27ec1
Salvador Candel <salvacandel@gmail.com>
2022-04-11 21:42:55 +0200

    Updating editors

--- editor/src/main/java/com/kreitek/editor/editors/ConsoleEditor.java
+++ editor/src/main/java/com/kreitek/editor/editors/ConsoleEditor.java
@@ -1,42 +1,11 @@
 package com.kreitek.editor.editors;
 
-import com.kreitek.editor.Document;
-import com.kreitek.editor.commands.CommandFactory;
-import com.kreitek.editor.exceptions.BadCommandException;
-import com.kreitek.editor.exceptions.ExitException;
-import com.kreitek.editor.interfaces.Command;
-import com.kreitek.editor.interfaces.Editor;
-
 import java.util.ArrayList;
-import java.util.Scanner;
 
-public class ConsoleEditor implements Editor {
-    public static final String TEXT_RESET = "\u001B[0m";
-    public static final String TEXT_RED = "\u001B[31m";
-    public static final String TEXT_YELLOW = "\u001B[33m";
-
-    private final CommandFactory commandFactory = new CommandFactory();
-    private Document document = new Document();
+public class ConsoleEditor extends Editor {
 
     @Override
-    public void run() {
-        boolean exit = false;
-        while (!exit) {
-            String commandLine = waitForNewCommand();
-            try {
-                Command command = commandFactory.getCommand(commandLine);
-                command.execute(document);
-            } catch (BadCommandException e) {
-                printErrorToConsole("Bad command");
-            } catch (ExitException e) {
-                exit = true;
-            }
-            showDocumentLines(document);
-            showHelp();
-        }
-    }
-
-    private void showDocumentLines(Document document) {
+    protected void showDocumentLines() {
         ArrayList<String> textLines = document.peek();
         if (textLines.size() > 0){
             setTextColor(TEXT_YELLOW);
@@ -52,36 +21,6 @@ public class ConsoleEditor implements Editor {
             printLnToConsole("<== END DOCUMENT");
             setTextColor(TEXT_RESET);
         }
-    }
-
-    private String waitForNewCommand() {
-        printToConsole("Enter a command : ");
-        Scanner scanner = new Scanner(System. in);
-        return scanner.nextLine();
-    }
-
-    private void showHelp() {
-        printLnToConsole("To add new line -> a \"your text\"");
-        printLnToConsole("To update line  -> u [line number] \"your text\"");
-        printLnToConsole("To delete line  -> d [line number]");
-    }
-
-    private void printErrorToConsole(String message) {
-        setTextColor(TEXT_RED);
-        printToConsole(message);
-        setTextColor(TEXT_RESET);
-    }
-
-    private void setTextColor(String color) {
-        System.out.println(color);
-    }
-
-    private void printLnToConsole(String message) {
-        System.out.println(message);
-    }
-
-    private void printToConsole(String message) {
-        System.out.print(message);
     }
 
 }
--- editor/src/main/java/com/kreitek/editor/editors/EditorFactory.java
+++ editor/src/main/java/com/kreitek/editor/editors/EditorFactory.java
@@ -1,9 +1,15 @@
 package com.kreitek.editor.editors;
 
-import com.kreitek.editor.interfaces.Editor;
+import com.kreitek.editor.exceptions.EditorException;
 
 public class EditorFactory {
-    public Editor getEditor()  {
-        return new ConsoleEditor();
+    public Editor getEditor(String[]args) throws EditorException {
+        if(args.length>0){
+            switch (args[0]){
+                case "text": return new ConsoleEditor();
+                case "json": return new JsonEditor();
+            }
+        }
+        throw new EditorException();
     }
 }
