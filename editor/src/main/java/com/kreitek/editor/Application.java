fde3a2e4903b6285e15a60bc36cab1f87ba1aeb1
Salvador Candel <salvacandel@gmail.com>
2022-04-11 21:44:04 +0200

    Updating Application

--- editor/src/main/java/com/kreitek/editor/Application.java
+++ editor/src/main/java/com/kreitek/editor/Application.java
@@ -1,14 +1,20 @@
 package com.kreitek.editor;
 
 import com.kreitek.editor.editors.EditorFactory;
-import com.kreitek.editor.interfaces.Editor;
+import com.kreitek.editor.editors.Editor;
+import com.kreitek.editor.exceptions.EditorException;
 
 public class Application {
 
     public static void main(String[] args) {
         EditorFactory editorFactory = new EditorFactory();
-        Editor editor = editorFactory.getEditor();
-        editor.run();
+        Editor editor;
+        try {
+            editor = editorFactory.getEditor(args);
+            editor.run();
+        }catch (Exception | EditorException e){
+            System.out.println("Incorrect document type");
+        }
     }
 
 }
